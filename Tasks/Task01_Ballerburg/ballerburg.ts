72e57ed0c74f3f84dd2ff97c1efdae7838c55b9e
jenny <jenny.w0400@gmail.com>
2024-10-22 14:01:05 +0200

    idfk hab irgendendwas gemacht und vergessen

--- Tasks/Task01_Ballerburg/ballerburg.ts
+++ Tasks/Task01_Ballerburg/ballerburg.ts
@@ -22,6 +22,9 @@ let pressedKey:string="";
 let cannonMouse:Cannon;
 let cannonKeyboard:Cannon;
 
+const keyboardAngle:HTMLElement=document.getElementById("angle1")!;
+
+
 
 
 window.addEventListener("load",handleLoad);
@@ -33,9 +36,65 @@ function handleLoad(_event:Event):void{
 
     //add event listeners
     document.addEventListener("keydown",processKeyboardInput);
-    
+    for(let i:number=0;i<=(document.getElementsByTagName("input")).length;i++){
+        const slider:HTMLInputElement=document.getElementsByTagName("input")[i];
+        switch(slider.id){
+            case "angle1":
+                slider.oninput=handleSlider();
+                break;
+            case "How to Play":
+                button.addEventListener("click",displayHelp);
+                break;
+            case "Restart":
+                button.addEventListener("click",restartGame);
+                break;
+            default:
+                console.log("extra button????????");
+                break;
+        }
+    }
+    for(let i:number=0;i<=(document.getElementsByTagName("button")).length;i++){
+        const button:HTMLElement=document.getElementsByTagName("button")[i];
+        switch(button.innerHTML){
+            case "Shoot":
+                button.addEventListener("click",shoot);
+                break;
+            case "How to Play":
+                button.addEventListener("click",displayHelp);
+                break;
+            case "Restart":
+                button.addEventListener("click",restartGame);
+                break;
+            default:
+                console.log("extra button????????");
+                break;
+        }
+    }
+
 }
 
 function processKeyboardInput(_event:KeyboardEvent):void{
     pressedKey=_event.key;
+}
+
+function handleSlider(_event:InputEvent,):void{
+    const target:EventTarget=_event.currentTarget!;
+    
+
+}
+
+function shoot(_event:MouseEvent):void{
+
+}
+
+function displayHelp(_event:MouseEvent):void{
+
+}
+
+function restartGame(_event:MouseEvent):void{
+
+}
+
+keyboardAngle.onchange=function keyboardAngleChange():void{
+    keyboardAngle.
 }
