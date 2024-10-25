6a0cd92e4a34b436ce03f4f9fc5edcf44a9c6488
Jussie247 <justus.schultzke@gmail.com>
2024-10-25 11:11:21 +0200

    setup Farm class

--- Tasks/Task02_Classes/farm/farm.js
+++ Tasks/Task02_Classes/farm/farm.js
@@ -1,2 +1,15 @@
 "use strict";
+//Class for Animal
+class Animal {
+    constructor(_name, _species, _food, _foodConsumption, _sound) {
+        this.name = _name;
+        this.species = _species;
+        this.food = _food;
+        this.foodConsumption = _foodConsumption;
+        this.sound = _sound;
+    }
+    sing() {
+        return this.name + " the " + this.species + " sings: Old MacDonald had a " + this.species + ", E-I-E-I-O! With a " + this.sound + "-" + this.sound + " here and a" + this.sound + "-" + this.sound + "there...";
+    }
+}
 //# sourceMappingURL=farm.js.map
--- Tasks/Task02_Classes/farm/farm.ts
+++ Tasks/Task02_Classes/farm/farm.ts
@@ -2,22 +2,37 @@
 //Class for Animal
 class Animal {
     name: string;
-    type: string;
+    species: string;
     food: string;
     foodConsumption: number;
     sound: string;
 
 
-    constructor(name: string, type: string, food: string, foodConsumption: number, sound: string) {
+    constructor(_name: string, _species: string, _food: string, _foodConsumption: number, _sound: string) {
 
-        this.name = name;
-        this.type = type;
-        this.food = food;
-        this.foodConsumption = foodConsumption;
-        this.sound = sound;
+        this.name = _name;
+        this.species = _species;
+        this.food = _food;
+        this.foodConsumption = _foodConsumption;
+        this.sound = _sound;
     }
     sing(): string {
-        return this.name + " the " + this.type + " sings: Old MacDonald had a " + this.type + ", E-I-E-I-O! With a " + this.sound + "-" + this.sound + " here...";
+        return this.name + " the " + this.species + " sings: Old MacDonald had a " + this.species + ", E-I-E-I-O! With a " + this.sound + "-" + this.sound + " here and a" + this.sound + "-" + this.sound + "there...";
     }
 
+    eat(_farm: Farm): string {
+
+    }
+
+}
+
+class Farm {
+    animals: Animal[] = [];
+    foodSupplies: { [key: string]: number };
+
+    constructor() {
+        this.foodSupplies = { "Grass": 100, "Grains": 100, "Meat": 50, "Junk": 30 };
+        this.loadAnimals();
+        this.installClickListener();
+    }
 }
