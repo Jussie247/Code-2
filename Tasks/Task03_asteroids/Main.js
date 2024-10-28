407bc37a35f7903410950b40961dfe15d430df65
“Jussie247” <justus.schultzke@gmail.com>
2024-10-28 20:39:52 +0100

    finished: Classes: Implementation Asteroid

--- Tasks/Task03_asteroids/Main.js
+++ Tasks/Task03_asteroids/Main.js
@@ -10,10 +10,16 @@ var T03_Asteroids;
         T03_Asteroids.crc2 = canvas.getContext("2d");
         T03_Asteroids.crc2.fillStyle = "black";
         T03_Asteroids.crc2.strokeStyle = "white";
+        T03_Asteroids.crc2.fillRect(0, 0, T03_Asteroids.crc2.canvas.width, T03_Asteroids.crc2.canvas.height);
         T03_Asteroids.createPaths();
         console.log("Asteroids paths: ", T03_Asteroids.asteroidPaths);
         let asteroid = new T03_Asteroids.Asteroid(1);
         console.log(asteroid);
+        for (let i = 0; i < 100; i++) {
+            T03_Asteroids.crc2.fillRect(0, 0, T03_Asteroids.crc2.canvas.width, T03_Asteroids.crc2.canvas.height);
+            asteroid.draw();
+            asteroid.move(0.1);
+        }
     }
 })(T03_Asteroids || (T03_Asteroids = {}));
 //# sourceMappingURL=Main.js.map
--- Tasks/Task03_asteroids/Main.ts
+++ Tasks/Task03_asteroids/Main.ts
@@ -11,6 +11,8 @@ namespace T03_Asteroids {
         crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
         crc2.fillStyle = "black";
         crc2.strokeStyle = "white";
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
 
         createPaths();
         console.log("Asteroids paths: ", asteroidPaths);
@@ -19,5 +21,12 @@ namespace T03_Asteroids {
         let asteroid: Asteroid = new Asteroid(1);
         console.log(asteroid);
 
+        for (let i: number = 0; i < 100; i++) {
+
+            crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+            asteroid.draw();
+            asteroid.move(0.1);
+
+        }
     }
 }
