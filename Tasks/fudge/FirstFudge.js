1fcd2d4d59814a3e5917c5573e0977860204beb4
Jussie247 <justus.schultzke@gmail.com>
2024-11-05 16:06:37 +0100

    added gameloop

--- Tasks/fudge/FirstFudge.js
+++ Tasks/fudge/FirstFudge.js
@@ -4,19 +4,39 @@ var FirstFudge;
     var f = FudgeCore;
     console.log(f);
     window.addEventListener("load", start);
+    const node = new f.Node("Node");
+    let globalViewport;
+    f.Loop.addEventListener("loopFrame" /* f.EVENT.LOOP_FRAME */, moveCube);
     function start() {
         const canvas = document.querySelector("canvas");
         console.log(canvas);
-        const cube = new f.MeshCube("Cube");
-        console.log(cube);
+        const mesh = new f.MeshCube("Cube");
+        console.log(mesh);
         const camera = new f.ComponentCamera();
         console.log(camera);
-        /*  const branch: f.Node = new f.Node("Node");
-         console.log(branch); */
-        const node = new f.Node("Node2");
+        const cmpMesh = new f.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        const material = new f.Material("Material", f.ShaderLit);
+        const cmpMaterial = new f.ComponentMaterial(material);
+        cmpMaterial.clrPrimary.set(1, 0.4, 0.7, 1);
+        node.addComponent(cmpMaterial);
+        camera.mtxPivot.translateZ(5);
+        camera.mtxPivot.rotateY(180);
+        const cpmTransform = new f.ComponentTransform();
+        node.addComponent(cpmTransform);
+        node.mtxLocal.translateX(0);
         console.log(node);
         const viewport = new f.Viewport();
         viewport.initialize("viewport", node, camera, canvas);
+        viewport.draw();
+        globalViewport = viewport;
+        console.log(viewport);
+        f.Loop.start();
+    }
+    function moveCube() {
+        console.log("move Cube");
+        node.mtxLocal.rotateY(20);
+        globalViewport.draw();
     }
 })(FirstFudge || (FirstFudge = {}));
 //# sourceMappingURL=FirstFudge.js.map
--- Tasks/fudge/FirstFudge.ts
+++ Tasks/fudge/FirstFudge.ts
@@ -5,25 +5,55 @@ namespace FirstFudge {
 
     window.addEventListener("load", start);
 
+
+    const node: f.Node = new f.Node("Node");
+    let globalViewport: f.Viewport;
+    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, moveCube);
+
+
     function start(): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
-
         console.log(canvas);
 
-        const cube: f.Mesh = new f.MeshCube("Cube");
-        console.log(cube);
+        const mesh: f.Mesh = new f.MeshCube("Cube");
+        console.log(mesh);
 
         const camera: f.ComponentCamera = new f.ComponentCamera();
         console.log(camera);
 
-        /*  const branch: f.Node = new f.Node("Node");
-         console.log(branch); */
+        const cmpMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
 
-        const node: f.Node = new f.Node("Node2");
+        const material: f.Material = new f.Material("Material", f.ShaderLit);
+        const cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(material);
+        cmpMaterial.clrPrimary.set(1, 0.4, 0.7, 1);
+        node.addComponent(cmpMaterial);
+
+
+        camera.mtxPivot.translateZ(5);
+        camera.mtxPivot.rotateY(180);
+
+
+        const cpmTransform: f.ComponentTransform = new f.ComponentTransform();
+        node.addComponent(cpmTransform);
+        node.mtxLocal.translateX(0);
+
         console.log(node);
 
         const viewport: f.Viewport = new f.Viewport();
         viewport.initialize("viewport", node, camera, canvas);
+        viewport.draw();
+        globalViewport = viewport;
+        console.log(viewport);
+
+        f.Loop.start();
+
+    }
+
+    function moveCube(): void {
+        console.log("move Cube");
+        node.mtxLocal.rotateY(20);
+        globalViewport.draw();
 
     }
 }
